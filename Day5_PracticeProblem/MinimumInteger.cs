b1e26f4d2b1c91a438c0d2942eb8378ae671c114
MansiRavindraMohan <manasimohan29@gmail.com>
2023-05-20 14:36:19 +0530

    Add: Program for finding minimum integer

---
+++ Day5_PracticeProblem/MinimumInteger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_PracticeProblem
+{
+    public class MinimumInteger
+    {
+        public void IntegerMethod()
+        {
+            int numOne = 80;
+            int numTwo = 50;
+            int numThree = 10;
+
+            int min = FindMinimum(numOne, numTwo, numThree);
+            Console.WriteLine("Minimum value: " + min);
+        }
+        public static T FindMinimum<T>(T valueOne, T valueTwo, T valueThree) where T : IComparable<T>
+        {
+            T min = valueOne;
+
+            if (valueTwo.CompareTo(min) < 0)
+                min = valueTwo;
+
+            if (valueThree.CompareTo(min) < 0)
+                min = valueThree;
+
+            return min;
+        }
+    }
+}
---
+++ Day5_PracticeProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_PracticeProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MinimumInteger minimumInteger = new MinimumInteger();
+            minimumInteger.IntegerMethod();
+            Console.ReadLine();
+        }
+    }
+}
